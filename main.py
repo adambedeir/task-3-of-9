e19d0669e421bda9f78082b32a74a10b0d7be8ba
adambedeir <adambedeir@gmail.com>
2020-07-13 16:39:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+answer=input('pick a number \n')
+if answer > 10
+  print('your number is larger than ten.')
+if answer< 10
+  print('your number is smaller than ten.')
+if answer== 10
+  print('you picked the number ten!')
